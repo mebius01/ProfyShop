20e25960646c51a53240b5f5aa0294729042423f
iv <consmebius@gmail.com>
2020-06-18 23:56:46 +0300

    header..

--- src/js/index.js
+++ src/js/index.js
@@ -4,18 +4,33 @@ import "../img/logo.png"
 import "../img/icons8-facebook-64.png"
 import "../img/icons8-instagram-64.png"
 import "../img/icons8-map-64.png"
+import "../img/icons8-phone-64.png"
 import "../img/icons8-pickup-point-64.png"
+import "../img/icons8-phone-bubble-64.png"
+
+import "../img/icons8-menu-64.png"
 
 console.log("hell world");
 
-facebook.onclick = function () {
-  this.classList.toggle('rotateY')
+const map = document.getElementById('map')
+const mapIcon = document.getElementById('mapIcon')
+
+mapIcon.addEventListener('click', showeMap)
+
+function showeMap(e) {
+  e.preventDefault();
+  console.log(map);
+  map.classList.toggle('hidden')
 }
 
-insta.onclick = function () {
-  this.classList.toggle('rotateX')
-}
+// facebook.onclick = function () {
+//   this.classList.toggle('rotateY')
+// }
 
-map.onclick = function () {
-  this.classList.toggle('rotate')
-}
+// insta.onclick = function () {
+//   this.classList.toggle('rotateX')
+// }
+
+// map.onclick = function () {
+//   this.classList.toggle('rotate')
+// }
